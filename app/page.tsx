288b40aaaf800b9c4792e2e91f3734854a53cd9c
Mrinal Pramanick <mrinalbitsat@gmail.com>
2023-12-07 00:20:03 +0530

    Setting up Auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
+import SignInBtn from '@/components/buttons/SignInBtn'
+import ThemeBtn from '@/components/buttons/ThemeBtn'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <div className='w-full h-screen flex flex-col'>
       <header className='py-6 px-4 border-b-2 dark:border-[#3c3c3c]'>
-        <div></div>
+        <div className='flex item-center justify-between container mx-auto'>
+          <ThemeBtn />
+          <SignInBtn />
+        </div>
       </header>
       <main></main>
       <footer></footer>
---
+++ components/buttons/SignInBtn.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+const SignInBtn = () => {
+
+    const signIn = () => {}
+  return (
+    <button
+      onClick={() => signIn()}
+      className="btn btn-circle dark:text-white dark:bg-neutral text-neutral bg-transparent hover:text-white"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="w-6 h-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9"
+        />
+      </svg>
+    </button>
+  );
+};
+
+export default SignInBtn;
