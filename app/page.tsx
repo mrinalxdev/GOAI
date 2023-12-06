b10a45e6c60083a6258839e5076083125d69dcd2
Mrinal Pramanick <mrinalbitsat@gmail.com>
2023-12-06 23:10:50 +0530

    Initial : DaisyUI setup

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className='w-full h-screen flex flex-col'>
+      <header className='py-6 px-4 border-b-2 dark:border-[#3c3c3c]'>
+        <div></div>
+      </header>
+      <main></main>
+      <footer></footer>
+    </div>
+  )
+}
